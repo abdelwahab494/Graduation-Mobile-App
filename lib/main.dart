9ba1a7d34b9b92ac198b31408b13aa0bf7a3dcc2
Abdelwahab <aaaaaaaaaab743@gmail.com>
2025-06-20 14:21:22 +0300

    Hide Keys

--- lib/main.dart
+++ lib/main.dart
@@ -9,11 +9,11 @@ import 'package:provider/provider.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 Future<void> main() async {
-  Gemini.init(apiKey: "AIzaSyD9k7u5umJGhZr0CIQ_1pNWn-oOqOsls3Y");
+  Gemini.init(apiKey: "");
   await Supabase.initialize(
     anonKey:
-        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF5d2Nyb253em54bHF5b2JpYXhxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDcyNDQ1MjgsImV4cCI6MjA2MjgyMDUyOH0.b9YnN0ZvFOyPF7t5w0Fr9dsG7MAaUxoGA9ORUNb_wJk",
-    url: "https://qywcronwznxlqyobiaxq.supabase.co",
+        "",
+    url: "",
   );
   runApp(
     MultiProvider(
